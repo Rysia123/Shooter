2ab57494cb2ca525095158bf4674a0082adde70d
Computer Science <polasz0409@gmail.com>
2024-09-28 11:41:55 +0200

    dodalam enemy

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 
 
 
@@ -14,10 +19,15 @@ public class Main extends Application {
     static final int WIDTH = 1200;
     static final int HEIGHT = 800;
     Circle shooter = new Circle(100, HEIGHT - 100, 70);
+    Image enemyImage = new Image("file:enemy-removebg-preview.png");
+    Image enemyImage2 = new Image("file:enemy-removebg-preview2.png");
+    //Circle enemy = new Circle(WIDTH - 100, 100, 70);
+    ImageView enemy = new ImageView(enemyImage);
+    Rectangle rectangle1 = new Rectangle(WIDTH - 300, HEIGHT - 100, 70, 20);
 
-    Circle enemy = new Circle(WIDTH - 100, 100, 70);
+
     int points = 0;
-    Text score = new Text(WIDTH - 100, HEIGHT - 100, points + "");
+    Text score = new Text(WIDTH - 100, HEIGHT - 60, points + "");
 
 
     public static void main(String[] args) {
@@ -31,9 +41,13 @@ public class Main extends Application {
         primaryStage.show();
         scene.setFill(Color.FLORALWHITE);
 
+        enemy.setLayoutY(100);
+        enemy.setLayoutX(WIDTH - 150);
 
-        root.getChildren().addAll(shooter, enemy, score);
-        sc
+        root.getChildren().addAll(shooter, enemy, score, rectangle1);
+        score.setFont(Font.font(30));
+
+
 
     }
 
